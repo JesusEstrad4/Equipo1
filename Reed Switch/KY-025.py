798d66cc790ea551d95bb4d271c241233c92a4e8
Jose Daniel Ramirez Bastidas <57473019+SoyBastidasGit@users.noreply.github.com>
2022-10-27 17:46:23 -0700

    Create KY-025.py

---
+++ Reed Switch/KY-025.py
@@ -0,0 +1,16 @@
+#Reed Switch KY-025
+#Jose Daniel Ramirez Bastidas
+
+import machine
+import time
+
+rw = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
+
+while True:
+    val1= rw.value()
+    time.sleep(0.01)
+    val2= rw.value()
+    if val1 and not val2:
+        print('Door Closed')
+    elif not val1 and val2:
+        print('Door Open')

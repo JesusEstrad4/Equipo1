017f358020e315a2380e64c092590ceae6e8c063
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2022-11-05 14:17:43 -0700

    Create main.py

---
+++ Analog Temp/main.py
@@ -0,0 +1,18 @@
+"""
+Omar Gamaliel Rodriguez Moreno
+Usuario Barny-Claus
+Estudiante del Instituto Tecnológico de Tijuana
+  Materia: Sistemas programables
+"""
+from machine import Pin
+import time,utime  
+
+lector=Pin(15,Pin.IN)  
+factorconversion = 3.3 / 65365
+
+while True:
+     
+    celsius = lector.read_u16() * factorconversion
+    temperatura = 27 - (celsius - 0.706) / 0.001721
+    print(temperatura)
+    utime.sleep(1)
